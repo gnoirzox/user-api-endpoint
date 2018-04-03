ea737c91599e89f00456742d08c477863c88a378
Simon ROUGER <simon.rouger@supinfo.com>
2018-04-03 01:54:32 +0100

    main.go ...

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
-package mainimport (	"log"	"net/http"	"github.com/gorilla/mux"	"./locations"	"./users")func main() {	router := mux.NewRouter()	router.HandleFunc("/user", users.PostUser).Methods("POST")	router.HandleFunc("/user/{id:[0-9]+}", users.GetUser).Methods("GET")	router.HandleFunc("/user/location", locations.PostLocation).Methods("POST")	log.Fatal(http.ListenAndServe(":8888", router))}
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.github.com/gorilla/mux"
+
+	"./locations"
+	"./users"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/user", user.PostUser).Methods("POST")
+	router.HandleFunc("/user/{id:[0-9]+}").Methods("GET")
+	router.HandleFunc("/user/location").Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8888", router))
+}
